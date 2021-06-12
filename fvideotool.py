5901f06524d41b1bd4c1f9e06c1e6da91015ec4e
Bilal Aloui <85788529+bilalalouime@users.noreply.github.com>
2021-06-12 12:49:19 +0100

    Update fvideotool.py

--- fvideotool.py
+++ fvideotool.py
@@ -4,8 +4,10 @@ import re
 
 # Don't use a samrtphone browser user-agent (i'm using firefox for windows user-agent)
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"}
+#Video url
+v_url = 'https://www.facebook.com/DimaRapTV/videos/moro-khofiatan/305658944351064/?_rdc=1&_rdr'
 # Send an http request using GET 
-r =requests.get('https://www.facebook.com/DimaRapTV/videos/moro-khofiatan/305658944351064/?_rdc=1&_rdr',headers=headers)
+r =requests.get(v_url,headers=headers)
 # Save response data as text
 html = r.text 
 
